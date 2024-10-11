a279a15d7838ad4a9a36d26cb35e67ab4794ac13
Luis Lima <103044626+LuisLimaJr@users.noreply.github.com>
2024-10-11 20:28:02 -0300

    pratica 5 - integração continua - fix
    pratica 5 - passo 6 - fix

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Hello, Worl";
+		return "Hello, World";
 	}
 	
 	@RequestMapping("/luis")
